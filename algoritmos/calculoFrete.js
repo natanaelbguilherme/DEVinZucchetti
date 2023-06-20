40e3303ee7991457299ee755808fa3788d896e50
natanaelbguilherme <natanaelbguilherme@gmail.com>
2023-06-20 11:47:35 -0300

    adciona funçoes de js nos botoes

--- algoritmos/calculoFrete.js
+++ algoritmos/calculoFrete.js
@@ -4,23 +4,56 @@
 var peso = document.querySelector('.pesoProduto')
 var distancia = document.querySelector('.distancia')
 var botao = document.querySelector('.botao')
+var botao2 = document.querySelector('.resetaFormulario')
+
+var input = document.querySelector('.pesoProduto')
+var input2 = document.querySelector('.distancia')
+var h1 = document.querySelector('h1')
 
 botao.addEventListener('click', calculaFrete)
 
+
 function calculaFrete(){
-  var custoPeso =  peso.value * 0.50
-  var custoDistancia = distancia.value * 0.10 
-  var custoFrete = custoPeso + custoDistancia
 
-  var custoFreteFormat = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(custoFrete)
+  if(input.value == 0 || input2.value == 0){
+    alert("Preencha todos os campos")
+  } else {
+      var custoPeso =  peso.value * 0.50
+      var custoDistancia = distancia.value * 0.10 
+      var custoFrete = custoPeso + custoDistancia
 
-  document.querySelector('span').innerHTML = custoFreteFormat
-}
+      var custoFreteFormat = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(custoFrete)
+
+      document.querySelector('h1').innerHTML = "Valor do Frete"
+      
+      input.style.display = 'none';
+      input2.style.display = 'none';
+      botao.style.display = 'none';
+
+      var newH2 = document.createElement('h2')
+      newH2.innerHTML = `O Valor do Frete é de ${custoFreteFormat} Reais` 
+      h1.after(newH2)
+
+      botao2.setAttribute('style', '')
+
+    }
+  }
+  
+  
 
 function reset(){
+  var h2 = document.querySelector('h2')
+
+  document.querySelector('h1').innerHTML = "Calcular Frete"
+
+  botao2.setAttribute('style', 'display: none')
+  h2.style.display = 'none';
+  botao.style.display = ''; 
+  peso.style.display = '';
+  distancia.style.display = '';
+
   document.querySelector('.pesoProduto').value = ""
   document.querySelector('.distancia').value = ""
-  document.querySelector('span').innerHTML = "R$ 0,00"
 }
 
 
