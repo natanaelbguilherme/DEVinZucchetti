df56d052fe9404987c37c3e446504d414db20fb5
natanaelbguilherme <natanaelbguilherme@gmail.com>
2023-06-20 10:07:54 -0300

    teste
    teste

--- algoritmos/calculoFrete.js
+++ algoritmos/calculoFrete.js
@@ -1,8 +1,29 @@
-var pesoProduto = Number(prompt("Digite o peso do produto"))
-var distancia = Number(prompt("Digite a distância do trajeto"))
+// var pesoProduto = Number(prompt("Digite o peso do produto"))
+// var distancia = Number(prompt("Digite a distância do trajeto"))
 
-var custoPeso =  pesoProduto * 0.50
-var custoDistancia = distancia * 0.10 
-var custoFrete = custoPeso + custoDistancia
+var peso = document.querySelector('.pesoProduto')
+var distancia = document.querySelector('.distancia')
+var botao = document.querySelector('.botao')
 
-console.log("O custo total é " + custoFrete + ' reais')
+botao.addEventListener('click', calculaFrete)
+
+function calculaFrete(){
+  var custoPeso =  peso.value * 0.50
+  var custoDistancia = distancia.value * 0.10 
+  var custoFrete = custoPeso + custoDistancia
+
+  var custoFreteFormat = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(custoFrete)
+
+  document.querySelector('span').innerHTML = custoFreteFormat
+}
+
+function reset(){
+  document.querySelector('.pesoProduto').value = ""
+  document.querySelector('.distancia').value = ""
+  document.querySelector('span').innerHTML = "R$ 0,00"
+}
+
+
+
+
+// console.log("O custo total é " + custoFrete + ' reais')
